39267e8aa8d610bb25901bca7a4f24ed90889b15
Nileshlejhro <nilesh.pushkarna@lejhro.com>
2023-12-20 17:17:13 +0530

    Remove the feature upload using input file

--- src/Components/ImageGallery.jsx
+++ src/Components/ImageGallery.jsx
@@ -29,23 +29,23 @@ const ImageGallery = () => {
     setIsHovered(false);
   };
   const currentTime = new Date().toLocaleString();
-  const addImageToGallery = (e) => {
-    // console.log(e.target.files[0].size);
-    setFilesData((pre) => {
-      const imgInfo = {
-        url: e.target.files[0].name,
-        uploadedTime: currentTime,
-        imgSize: parseInt(e.target.files[0].size / 1024),
-      };
-      dispatch(addImgToStore({
-        url: e.target.files[0].name,
-        uploadedTime: currentTime,
-        imgSize: parseInt(e.target.files[0].size / 1024)
-      }))
-      console.log(images)
-      return [...pre, imgInfo];
-    });
-  };
+  // const addImageToGallery = (e) => {
+  //   // console.log(e.target.files[0].size);
+  //   setFilesData((pre) => {
+  //     const imgInfo = {
+  //       url: e.target.files[0].name,
+  //       uploadedTime: currentTime,
+  //       imgSize: parseInt(e.target.files[0].size / 1024),
+  //     };
+  //     dispatch(addImgToStore({
+  //       url: e.target.files[0].name,
+  //       uploadedTime: currentTime,
+  //       imgSize: parseInt(e.target.files[0].size / 1024)
+  //     }))
+  //     console.log(images)
+  //     return [...pre, imgInfo];
+  //   });
+  // };
   const changeTheInputType = (e) => {
     // console.log(e.target);
     if (e.target.value.length > 0) {
@@ -111,58 +111,21 @@ const ImageGallery = () => {
       <div className="w-[85rem] h-auto bg-gray-200 ms-8">
         <h1 className="text-5xl text-center m-6">Image Gallery</h1>
         <div className="flex justify-center flex-wrap">
+        {/* //UPLOAD OPTION USING INPUT FILE TYPE FILE */}
           {/* <span><input className='appearance-none  w-96 bg-gray-200 text-gray-700  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' type='file' value="" name="uploadImg" /></span> */}
-          <div className="col-span-full">
+          {/* <div className="col-span-full">
             <label
               htmlFor="cover-photo"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
               Upload photo
             </label>
-            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-              <div className="text-center">
-                <svg
-                  className="mx-auto h-12 w-12 text-gray-300"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                {/* <div className="mt-4 flex text-sm leading-6 text-gray-600"> */}
-                <label
-                  htmlFor="file-upload"
-                  className="relative cursor-pointer rounded-md  font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                >
-                  <span>Upload a file</span>
-                  <input
-                    id="file-upload"
-                    name="file-upload"
-                    type="file"
-                    className="sr-only"
-                    ref={inputRef}
-                    onChange={addImageToGallery}
-                  />
-                </label>
-                {/* <p className="pl-1">or drag and drop</p>
-                </div> */}
-                <p className="text-xs leading-5 text-gray-600">
-                  PNG, JPG, GIF up to 10MB
-                </p>
-                  <p className="text-xs leading-5 text-gray-600 text-red-700">
-                    *If Due To Security Purpose The Upload File Will Be Not Able
-                    To Show So You Can Also Try To Image Url{" "}
-                  </p>
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center m-2">
+            
+          
+          </div> */}
+          {/* <div className="flex items-center m-2">
             <span>Or</span>
-          </div>
+          </div> */}
           <div className="flex items-center">
             <div className="sm:col-span-4">
               <label
@@ -312,3 +275,51 @@ const ImageGallery = () => {
 };
 
 export default ImageGallery;
+
+
+
+
+
+
+
+
+{/* <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"> */}
+              // <div className="text-center">
+                // <svg
+                //   className="mx-auto h-12 w-12 text-gray-300"
+                //   viewBox="0 0 24 24"
+                //   fill="currentColor"
+                //   aria-hidden="true"
+                // >
+                //   <path
+                //     fillRule="evenodd"
+                //     d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+                //     clipRule="evenodd"
+                //   />
+                // </svg>
+                {/* <div className="mt-4 flex text-sm leading-6 text-gray-600"> */}
+                {/* <label
+                  htmlFor="file-upload"
+                  className="relative cursor-pointer rounded-md  font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                >
+                  <span>Upload a file</span>
+                  <input
+                    id="file-upload"
+                    name="file-upload"
+                    type="file"
+                    className="sr-only"
+                    ref={inputRef}
+                    onChange={addImageToGallery}
+                  />
+                </label> */}
+                {/* <p className="pl-1">or drag and drop</p>
+                </div> */}
+                {/* <p className="text-xs leading-5 text-gray-600">
+                  PNG, JPG, GIF up to 10MB
+                </p>
+                  <p className="text-xs leading-5 text-gray-600 text-red-700">
+                    *If Due To Security Purpose The Upload File Will Be Not Able
+                    To Show So You Can Also Try To Image Url{" "}
+                  </p>
+              </div>
+            </div> */}
